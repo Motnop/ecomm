47616be8de528899ffb5872a11b6b46ac3bc7a6b
pontom <motnop@gmail.com>
2020-03-24 18:08:48 +0300

    some changes del bash new branch

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     DEBUG = True
     SECRET_KEY = "UYHA00212uehd89aifbu27i8eyd90uY*WGUF1WEBH23qSAssfJ"
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:1234@127.0.0.1:5432/ecomm"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
